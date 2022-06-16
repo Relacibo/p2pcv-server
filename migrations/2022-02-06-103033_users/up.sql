2c3171e0be3183e9006a11a92e0dbdfc81f65d38
Reinhard Bronner <reinhard_bronner@t-online.de>
2022-06-16 17:08:15 +0000

    user -> users

--- migrations/2022-02-06-103033_users/up.sql
+++ migrations/2022-02-06-103033_users/up.sql
@@ -1,17 +1,17 @@
 CREATE EXTENSION pgcrypto;
 CREATE SCHEMA auth;
-CREATE TABLE auth.user (
+CREATE TABLE auth.users (
   id UUID DEFAULT gen_random_uuid() PRIMARY KEY,
   name VARCHAR(20) NOT NULL,
   email VARCHAR(32) NOT NULL UNIQUE,
   created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
-SELECT diesel_manage_updated_at('auth.user');
-ALTER TABLE auth.user ENABLE ROW LEVEL SECURITY;
-CREATE POLICY users_access_account ON auth.user USING (id = current_setting('jwt.claims.user')::uuid);
+SELECT diesel_manage_updated_at('auth.users');
+ALTER TABLE auth.users ENABLE ROW LEVEL SECURITY;
+CREATE POLICY users_access_account ON auth.users USING (id = current_setting('jwt.claims.user')::uuid);
 CREATE VIEW auth.user_view AS
 SELECT id,
   name,
   created_at
-FROM auth.user;
+FROM auth.users;
--- migrations/2022-06-16-095715_google/up.sql
+++ migrations/2022-06-16-095715_google/up.sql
@@ -1,6 +1,6 @@
 CREATE TABLE auth.google (
   id VARCHAR(255) PRIMARY KEY NOT NULL,
-  user_id UUID REFERENCES auth.user(id),
+  user_id UUID REFERENCES auth.users(id),
   created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 CREATE OR REPLACE FUNCTION auth.insert_login(
@@ -8,14 +8,14 @@ CREATE OR REPLACE FUNCTION auth.insert_login(
     param_email VARCHAR(32),
     param_google_id VARCHAR(255)
   )
-  RETURNS auth.user
+  RETURNS auth.users
   LANGUAGE plpgsql
   SECURITY DEFINER
   AS $$
   DECLARE
-    user_entry auth.user;
+    user_entry auth.users;
 BEGIN
-  INSERT INTO auth.user (name, email)
+  INSERT INTO auth.users (name, email)
     VALUES (param_name, param_email)
     RETURNING * 
     INTO user_entry;
@@ -27,13 +27,13 @@ $$;
 CREATE OR REPLACE FUNCTION auth.lookup_user_with_google(
     param_google_id VARCHAR(255)
   ) 
-  RETURNS auth.user 
+  RETURNS auth.users 
   LANGUAGE plpgsql 
   SECURITY DEFINER 
   AS $$ 
 BEGIN
   RETURN (SELECT u FROM auth.google g 
-    INNER JOIN auth.user u 
+    INNER JOIN auth.users u 
       ON g.user_id = u.id
       AND g.id = param_google_id);
 END;
