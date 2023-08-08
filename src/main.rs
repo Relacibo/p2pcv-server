6a6c900bdb0c3d919237cc63de56943a07f49ba4
Reinhard Bronner <reinhard_bronner@t-online.de>
2023-08-08 12:05:17 +0000

    revert debug changes

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,18 @@
 use db::db_conn::DbPool;
 use diesel_async::{pooled_connection::AsyncDieselConnectionManager, AsyncPgConnection};
 use dotenvy::dotenv;
-extern crate diesel;
 extern crate bb8;
+extern crate diesel;
 #[macro_use]
 extern crate actix_web;
 #[macro_use]
 extern crate serde_with;
 extern crate env_logger;
-use actix_web::{middleware::Logger, web::{Data, self}, App, HttpServer, HttpResponse};
+use actix_web::{
+    middleware::Logger,
+    web::{self, Data},
+    App, HttpResponse, HttpServer,
+};
 use env_logger::Env;
 use log::debug;
 #[macro_use]
@@ -26,38 +30,27 @@ mod db;
 async fn main() -> std::io::Result<()> {
     dotenv().ok();
     env_logger::init_from_env(Env::default().default_filter_or("debug"));
-    // let database_url = env::var("DATABASE_URL").unwrap();
+    let database_url = env::var("DATABASE_URL").unwrap();
     let actix_host = env::var("ACTIX_HOST").expect("ACTIX_HOST not set!");
     debug!("actix web host: {actix_host}");
     let actix_port = env::var("ACTIX_PORT").expect("ACTIX_PORT not set!");
     debug!("actix web port: {actix_port}");
-    // let manager = AsyncDieselConnectionManager::<AsyncPgConnection>::new(database_url);
-    // let pool: DbPool = bb8::Pool::builder()
-    //     .build(manager)
-    //     .await
-    //     .expect("Failed to create pool.");
+    let manager = AsyncDieselConnectionManager::<AsyncPgConnection>::new(database_url);
+    let pool: DbPool = bb8::Pool::builder()
+        .build(manager)
+        .await
+        .expect("Failed to create pool.");
 
-    // HttpServer::new(move || {
-    //     App::new()
-    //         .service(
-    //             // Health check
-    //             web::resource("/").route(web::get().to(HttpResponse::Ok)),
-    //         )
-    //         .configure(api::auth::jwt::config)
-    //         .configure(api::users::config)
-    //         .configure(api::auth::config)
-    //         .app_data(Data::new(pool.clone()))
-    //         .wrap(Logger::default())
-    // })
-    // .bind(format!("{actix_host}:{actix_port}"))?
-    // .run()
-    // .await
     HttpServer::new(move || {
         App::new()
             .service(
                 // Health check
                 web::resource("/").route(web::get().to(HttpResponse::Ok)),
             )
+            .configure(api::auth::jwt::config)
+            .configure(api::users::config)
+            .configure(api::auth::config)
+            .app_data(Data::new(pool.clone()))
             .wrap(Logger::default())
     })
     .bind(format!("{actix_host}:{actix_port}"))?
