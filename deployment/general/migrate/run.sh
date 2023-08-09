087552aa0556a380f6ee961b927bffbf9f70c682
Reinhard Bronner <reinhard_bronner@t-online.de>
2023-08-09 19:17:20 +0200

    migration script exit success

--- deployment/general/migrate/run.sh
+++ deployment/general/migrate/run.sh
@@ -7,3 +7,12 @@ git sparse-checkout set migrations
 git checkout $GIT_COMMIT
 
 diesel migration run
+
+echo "Ran migrations successfully!"
+echo "GIT_REPOSITORY: $GIT_REPOSITORY"
+echo "GIT_COMMIT: $GIT_COMMIT"
+echo "PGUSER: $PGUSER"
+echo "PGHOST: $PGHOST"
+echo "PGPORT: $PGPORT"
+echo "PGDATABASE: $PGDATABASE"
+exit 0
