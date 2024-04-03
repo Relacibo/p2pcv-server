2abd614c5bde0a832917aa310599e4d827bfc9d6
Reinhard <reinhard_bronner@t-online.de>
2024-04-04 00:23:06 +0200

    wip

--- src/redis_db/peer_connections.rs
+++ src/redis_db/peer_connections.rs
@@ -17,7 +17,7 @@ impl PeerConnection {
     }
     pub async fn list_for_user(conn: &redis::aio::MultiplexedConnection, user_id: Uuid) {
         let minimum_last_ping = Utc::now() - Duration::minutes(1.5f32);
-        conn.get("*_user_id")
+        conn.get("@peer_id:[*]_user_id")
         conn.set(format!("{}_user_id", id), user_id);
         conn.set(format!("{}_updated_at", id), now);
     }
