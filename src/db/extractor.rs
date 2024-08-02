9043b4e169064487ed7089335154626311206a4e
Reinhard Bronner <reinhard_bronner@t-online.de>
2024-08-02 15:22:49 +0000

    fix db extractor

--- src/db/extractor.rs
+++ src/db/extractor.rs
@@ -16,14 +16,11 @@ impl FromRequest for DbConn {
 
     type Future = LocalBoxFuture<'static, Result<Self, Self::Error>>;
 
-    fn from_request(
-        req: &actix_web::HttpRequest,
-        payload: &mut actix_web::dev::Payload,
-    ) -> Self::Future {
+    fn from_request(req: &actix_web::HttpRequest, _: &mut actix_web::dev::Payload) -> Self::Future {
         let db_data = req
             .app_data::<Data<DbPool>>()
             .expect("Postgres Datapool not in data!");
-        let db_arc = db_data.into_inner().clone();
+        let db_arc = db_data.clone().into_inner();
         Box::pin(async move {
             let db = db_arc.get_owned().await?;
             Ok(DbConn(db))
